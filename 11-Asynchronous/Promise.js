dcd8e85afe4c5a53032eaa4b8901aa603a12e150
Astitva Shukla <astitvashukla1605@gmail.com>
2025-09-20 14:35:44 +0530

    Implement async/await for fetching country data and update HTML button

--- 11-Asynchronous/Promise.js
+++ 11-Asynchronous/Promise.js
@@ -1,21 +1,40 @@
 // XML Format of sending req and requests lead to callback help if one request depends on the data recieved by the prev req using nesting
 // To solve this problem we use fetch and Promises
-const res1 = fetch('https://restcountries.com/v3.1/name/india'); // correct endpoint
-const res2 = fetch('https://restcountries.com/v2/name/india');
+// const res1 = fetch('https://restcountries.com/v3.1/name/india'); // correct endpoint
+// const res2 = fetch('https://restcountries.com/v2/name/india');
 // console.log(res1, res2);
 // these reutrn promises: They are like placeholders that are used for future result of asynchronus operations
 // Promising gets rid of nesting as it can be chianed
 
 //consume prmomise
 
-const res3 = function () {
-  fetch('https://restcountries.com/v3.1/name/meow')
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    })
-    .catch((err) => console.log(err.message));
+// const res3 = function () {
+//   fetch('https://restcountries.com/v3.1/name/portugal')
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     })
+//     .catch((err) => console.log('an error occured'))
+//     .finally(() => console.log('finally'));
+// };
+
+// Async await
+// try catch
+
+whereAmI = async function (country) {
+  try {
+    const res = await fetch(`https://restcountries.com/v2/name/${country}`);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(` ********* an error occured ${err.message} ${err}`);
+  }
 };
-res3();
+
+console.log('I am Trying something');
+
+document.querySelector('.btn').addEventListener('click', () => {
+  whereAmI('india');
+});
